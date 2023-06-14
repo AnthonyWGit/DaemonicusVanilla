b2fc031547c3b2e4ee57e06f3491db5bca3c0053
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-14 17:36:39 +0200

    separated models to get data from db

---
+++ src/models/UserDataRetrieval.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Models;
+
+class UserDataRetrieval
+{
+    function getOneRowUser($userId)
+    {
+        //------------------------SQL request-----------------------------------
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn_joueur
+                    WHERE  id_joueur= :id'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+        $stmt->bindValue(':id', $userId);
+        $stmt->execute();                                                     //The data we retrieve are in array form
+        $user = $stmt->fetchAll();
+        unset($stmt);
+        return $user;
+    }
+
+    function getUserId()
+    {
+        //------------------------SQL request-----------------------------------
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT id_joueur FROM joueur
+                    WHERE  pseudo_joueur = :username'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+        $stmt->bindValue(':username', $_SESSION["username"]);
+        $stmt->execute();                                                     //The data we retrieve are in array form
+        $user = $stmt->fetchAll();
+        unset($stmt);
+        return $user;
+    }
+}
