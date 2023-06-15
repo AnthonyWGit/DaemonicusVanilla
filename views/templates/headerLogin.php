52d6f5cb55d2976c8093dfa64b6dc118e6728bb6
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-15 12:39:21 +0200

    disconnect hidden while not logged in

--- views/templates/headerLogin.php
+++ views/templates/headerLogin.php
@@ -7,7 +7,7 @@
                 <h3><?php if(isset($_SESSION["username"]) && $_SESSION["session"]) echo "Hello ".$_SESSION["username"] ?></h3>
             </div>
             <button class="loginBtn"><a href="index.php?action=displayLogin"> Login</a></button>
-            <button class="loginBtn"><a href="index.php?action=disconnect">disconnect</a></button>
+            <?= isset($_SESSION["username"]) && $_SESSION["session"] ?  '<button class="loginBtn"><a href="index.php?action=disconnect">disconnect</a></button>' : '' ?>
         </div>
     </div>
 </header>
