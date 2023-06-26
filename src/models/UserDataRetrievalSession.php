a16eb75c376e93518d5252b193e4ffe691db5948
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-26 16:15:59 +0200

    fuction retrieve pkmn player order 1

--- src/models/UserDataRetrievalSession.php
+++ src/models/UserDataRetrievalSession.php
@@ -19,7 +19,7 @@ class UserDataRetrievalSession
         return $user;
     }
 
-    function getPkmnPlayer()
+    static function getPkmnPlayer()
     {
         //------------------------SQL request-----------------------------------
         $mySQLconnection = Connect::connexion();
@@ -27,6 +27,33 @@ class UserDataRetrievalSession
                     WHERE  id_joueur= :id'; 
         $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
         $stmt->bindValue(':id', $_SESSION["userID"]);
+        $stmt->execute();                                                     //The data we retrieve are in array form
+        $user = $stmt->fetchAll();
+        unset($stmt);
+        return $user;
+    }
+
+    static function getPkmnPlayerOrderOne()
+    {
+        //------------------------SQL request-----------------------------------
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn_joueur INNER JOIN pkmn ON pkmn_joueur.id_pkmn = pkmn.id_pkmn
+                    WHERE  id_joueur= :id AND ordre_pkmn = 1'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
+        $stmt->bindValue(':id', $_SESSION["userID"]);
+        $stmt->execute();                                                     //The data we retrieve are in array form
+        $user = $stmt->fetchAll();
+        unset($stmt);
+        return $user;
+    }
+
+    static function setCPUPkmn()
+    {
+        //------------------------SQL request-----------------------------------
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn
+                    WHERE  nom_pkm = "Imp"'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
         $stmt->execute();                                                     //The data we retrieve are in array form
         $user = $stmt->fetchAll();
         unset($stmt);
