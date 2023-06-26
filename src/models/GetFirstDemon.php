1960ea9d1be1a1dc2560d046ffa5df3cdc40baa3
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-26 16:15:19 +0200

    first demon is lvl 4

--- src/models/GetFirstDemon.php
+++ src/models/GetFirstDemon.php
@@ -12,13 +12,14 @@ class GetFirstDemon
         $random = $ctrl->pickOneRandomizedCara();
         //------------------------SQL request-----------------------------------
         $mySQLconnection = Connect::connexion();
-        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur) 
-                    VALUES (:id_caractere, :id_pkmn, :id_joueur)';
+        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur, experience  ) 
+                    VALUES (:id_caractere, :id_pkmn, :id_joueur, :experience)';
         $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
         $array = [
                     'id_caractere' => $random,
                     'id_joueur' => $_SESSION["userID"],
-                    'id_pkmn' => 2
+                    'id_pkmn' => 2,
+                    'experience' => 5000
                 ];
         $stmt->execute($array);                                                     //The data we retrieve are in array form
         unset($stmt);
@@ -30,13 +31,14 @@ class GetFirstDemon
         $random = $ctrl->pickOneRandomizedCara();
         //------------------------SQL request-----------------------------------
         $mySQLconnection = Connect::connexion();
-        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur) 
-                    VALUES (:id_caractere, :id_pkmn, :id_joueur)';
+        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur, experience) 
+                    VALUES (:id_caractere, :id_pkmn, :id_joueur, :experience)';
         $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
         $array = [
                     'id_caractere' => $random,
                     'id_joueur' => $_SESSION["userID"],
-                    'id_pkmn' => 3
+                    'id_pkmn' => 3,
+                    'experience' => 5000
                 ];
         $stmt->execute($array);                                                     //The data we retrieve are in array form
         unset($stmt);
@@ -48,13 +50,14 @@ class GetFirstDemon
         $random = $ctrl->pickOneRandomizedCara();
         //------------------------SQL request-----------------------------------
         $mySQLconnection = Connect::connexion();
-        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur) 
-                    VALUES (:id_caractere, :id_pkmn, :id_joueur)';
+        $sqlQuery = 'INSERT INTO pkmn_joueur (id_caractere, id_pkmn, id_joueur, experience) 
+                    VALUES (:id_caractere, :id_pkmn, :id_joueur, :experience)';
         $stmt = $mySQLconnection->prepare($sqlQuery);                        //Prepare, execute, then fetch to retrieve data
         $array = [
                     'id_caractere' => $random,
                     'id_joueur' => $_SESSION["userID"],
-                    'id_pkmn' => 5
+                    'id_pkmn' => 5,
+                    'experience' => 5000
                 ];
         $stmt->execute($array);                                                     //The data we retrieve are in array form
         unset($stmt);
