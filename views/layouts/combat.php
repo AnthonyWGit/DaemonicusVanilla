812f35d1abe4e88777bac38b65a76b8cd1a7b3d1
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-26 16:16:18 +0200

    game combat template

---
+++ views/layouts/combat.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome !</title>
+        <link rel="stylesheet" href="public/styles/combat.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@300;400;500;600;700;800;900&display=swap">
+    </head>
+    <body>
+        <div class="wrapper">
+            <?php require "views/templates/headerHome.php" ?>
+                <?= $content ?>
+        </div>
+    </body>
+</html>
+
