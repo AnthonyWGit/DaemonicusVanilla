1893be98c3d4ff7ee38ac0ab9bb8bcfbd20cf2d5
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-29 12:58:40 +0200

    retrieving traits

--- src/models/UserDataRetrievalSession.php
+++ src/models/UserDataRetrievalSession.php
@@ -2,6 +2,7 @@
 
 namespace Models;
 use Models\Connect;
+use Models\CCC;
 
 class UserDataRetrievalSession
 {
@@ -60,16 +61,124 @@ class UserDataRetrievalSession
         return $user;
     }
 
-    static function startNewCombat();
+    static function getCPUID()
     {
         //------------------------SQL request-----------------------------------
         $mySQLconnection = Connect::connexion();
-        $sqlQuery = 'INSERT INTO combat(pv_pkmn1, pv_pkmn2'; 
-        $stmt = $mySQLconnection->prepare($sqlQuery);                    
-        $stmt->bindValue(':id', $_SESSION["userID"]);
-        $stmt->execute();                                                 
-        $user = $stmt->fetchAll();
+        $sqlQuery = 'SELECT * FROM joueur
+                    WHERE pseudo_joueur  = "CPU"'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                       
+        $stmt->execute();                                                     
+        $CPUId = $stmt->fetchAll();
         unset($stmt);
-        return $user;
+        return $CPUId;
     }
+
+    static function getCPUIDFirstPKmn()
+    {
+        $cpuID = UserDataRetrievalSession::getCPUID();
+        $cpuID2 = $cpuID[0]["id_joueur"];
+        
+        // SQL request
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn_joueur WHERE id_joueur = :id_joueur'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);
+        $stmt->bindValue(":id_joueur", $cpuID2, \PDO::PARAM_INT);
+        $stmt->execute(); // Execute the prepared statement
+        $CPUIdDaemon = $stmt->fetchAll();
+        
+        unset($stmt);
+        return $CPUIdDaemon;
+    }
+
+    static function startNewCombat($maxHPPlayer, $maxHPCPU)
+    {
+
+        $firstDeamon = UserDataRetrievalSession::getPkmnPlayerOrderOne();
+        UserDataRetrievalSession::generateCPUDaemon();
+
+        $daemonCPU = UserDataRetrievalSession::getCPUIDFirstPKmn();
+        $daemonCPU = $daemonCPU[0]["id_pkmn_joueur"];
+
+        $arrayToExecute = [
+            "pv_pkmn_1" => $maxHPPlayer, 
+            "pv_pkmn_2" => $maxHPCPU, 
+            "id_pkmn_joueur" => $firstDeamon[0]["id_pkmn_joueur"],  
+            "id_pkmn_joueur_1" => $daemonCPU];
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'INSERT INTO combat(pv_pkmn_1, pv_pkmn_2, id_pkmn_joueur, id_pkmn_joueur_1)
+                    VALUES (:pv_pkmn_1, :pv_pkmn_2, :id_pkmn_joueur, :id_pkmn_joueur_1)'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                 
+        $stmt->execute($arrayToExecute);                                             
+        unset($stmt);
+    }
+
+    static public function generateCPUDaemon()
+    {
+        $array = [0, 500];
+        $randomIndex = array_rand($array);
+        $randomValue = $array[$randomIndex];
+
+        $cpuID = UserDataRetrievalSession::getCPUID();
+
+        foreach ($cpuID as $key => $value)
+        {
+            if ($value["id_joueur"])
+            {
+                $cpuID2 = ($value["id_joueur"]);
+            }
+        }
+
+        $ctrl = new CCC();
+        $randomCara = $ctrl->pickOneRandomizedCara();
+
+        $arrayToExecute = [
+            "experience" => $array[$randomIndex], 
+            "id_caractere" => $randomCara, 
+            "id_joueur" => $cpuID2, 
+            "id_pkmn" => 6];
+        $mySQLconnection = Connect::connexion();
+
+        $sqlQuery = 'INSERT INTO pkmn_joueur(experience, id_caractere, id_joueur, id_pkmn) 
+                    VALUES (:experience,:id_caractere,:id_joueur, :id_pkmn)'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);                    
+        $stmt->execute($arrayToExecute);
+        $lastInsertID = $mySQLconnection->lastInsertId();
+        $_SESSION["id_CPU_daemon"] = $lastInsertID;                                     
+        unset($stmt);
+        return $lastInsertID;
+    }
+
+    static public function getPlayerDaemonStatsFirst() //id of player of unique so we can use it 
+    {
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn_joueur INNER JOIN joueur ON pkmn_joueur.id_joueur = joueur.id_joueur
+                    INNER JOIN pkmn on pkmn_joueur.id_pkmn = pkmn.id_pkmn 
+                    INNER JOIN caractere ON pkmn_joueur.id_caractere = caractere.id_caractere
+                    AND joueur.id_joueur = :id_joueur'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);
+        $stmt->bindValue(':id_joueur',$_SESSION['userID']);               
+        $stmt->execute();          
+        $stats = $stmt->fetchAll();
+        unset($stmt);
+        return $stats;
+    }
+
+    static public function getCPUDaemonStatsFirst() //we can't use cp id because it will show ALL the cpu pkmn
+                                                    //Multiple players = theyre will be a lot of entries 
+    {
+        $playerDaemon = UserDataRetrievalSession::getPlayerDaemonStatsFirst();
+        $playerDaemon = $playerDaemon[0]["id_pkmn_joueur"];
+        $mySQLconnection = Connect::connexion();
+        $sqlQuery = 'SELECT * FROM pkmn_joueur INNER JOIN joueur ON pkmn_joueur.id_joueur = joueur.id_joueur
+                    INNER JOIN pkmn on pkmn_joueur.id_pkmn = pkmn.id_pkmn 
+                    WHERE pkmn_joueur.id_pkmn_joueur = :id'; 
+        $stmt = $mySQLconnection->prepare($sqlQuery);
+        $stmt->bindValue(':id',$_SESSION["id_CPU_daemon"]);                       
+        $stmt->execute();                                                     
+        $stats = $stmt->fetchAll();
+        unset($stmt);
+        return $stats;
+    }
+
 }
