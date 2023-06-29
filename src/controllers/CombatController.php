4e05fca73be6345179f5169f2ae8ddfe64486917
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-06-29 12:58:13 +0200

    stats calculations

--- src/controllers/CombatController.php
+++ src/controllers/CombatController.php
@@ -12,21 +12,35 @@ class CombatController
 {
  public function startCombat()
  {
-    $daemon = UserDataRetrievalSession::getPkmnPlayerOrderOne();
-    $daemonCPU = UserDataRetrievalSession::setCPUPkmn();
-    $xpDaemonPlayer = $daemon[0]["experience"];
+   $daemon = UserDataRetrievalSession::getPkmnPlayerOrderOne();
+   $daemonCPU = UserDataRetrievalSession::setCPUPkmn();
+   $xpDaemonPlayer = $daemon[0]["experience"];
 
-    $daemonCPUMaxHP = 50;
-    $daemonPlayerMaxHP = Math::calcMaxHPPlayer();
-    $daemonPlayerLevel = Math::calculateLevel($xpDaemonPlayer);
+   $daemonCPUMaxHP = 50;
+   $daemonPlayerMaxHP = Math::calcMaxHPPlayer();
+   $daemonPlayerLevel = Math::calculateLevel($xpDaemonPlayer);
 
-    $daemonPlayerCurrentHP = $daemonPlayerMaxHP;
+   $daemonPlayerCurrentHP = $daemonPlayerMaxHP;
 
-    $daemonCPUCurrentHP = $daemonCPUMaxHP;
+   $daemonCPUCurrentHP = $daemonCPUMaxHP;
 
-    var_dump($xpDaemonPlayer);
-    var_dump($daemonPlayerLevel);
-    var_dump($daemonPlayerMaxHP);
-    require_once ("views/templates/gameCombat.php");
+   // var_dump($xpDaemonPlayer);
+   // var_dump($daemonPlayerLevel);
+   // var_dump($daemonPlayerMaxHP);
+
+   UserDataRetrievalSession::startNewCombat($daemonPlayerMaxHP, $daemonCPUMaxHP);
+
+   $statsPlayer = UserDataRetrievalSession::getPlayerDaemonStatsFirst();
+   $statsCPU = UserDataRetrievalSession::getCPUDaemonStatsFirst();
+
+   $forcePlayer = Math::calcFor();
+
+   var_dump($forcePlayer);
+
+   // var_dump($statsPlayer);
+   // var_dump($statsCPU);
+   // var_dump($_SESSION["id_CPU_daemon"]);
+   
+   require_once ("views/templates/gameCombat.php");
  }
 }
