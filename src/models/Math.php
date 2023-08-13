1d749824a6cadea124672f86c592c797087ebe69
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-08-13 14:41:36 +0200

    calculation fixes

--- src/models/Math.php
+++ src/models/Math.php
@@ -66,55 +66,56 @@ class Math
     public static function calcStatsPlayer()
     {
         $forcePKMNs = UserDataRetrievalSession::getPlayerDaemonStats();
+        var_dump($forcePKMNs);
         foreach ($forcePKMNs as $forcePKMN)
         {
             if ($forcePKMN["force_pkmn_2"] < 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] + ((1 / 100) * $forcePKMN["force_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] + ($forcePKMN["force_pkmn_2"] * 0.1));
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] - ((1 / 100) * $forcePKMN["force_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] - ($forcePKMN["force_pkmn_2"] * 0.1));
             }
-            if ($forcePKMN["agi_pkmn_2"] < 0)
+            if ($forcePKMN["agi_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] + ((1 / 100) * $forcePKMN["agi_pkmn_2"]));  
-            }
-            else
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] - ((1 / 100) * $forcePKMN["agi_pkmn_2"]));  
-            }
-            if ($forcePKMN["end_pkmn_2"] < 0)
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] + ((1 / 100) * $forcePKMN["end_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] + ($forcePKMN["agi_pkmn_2"] * 0.1));
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] - ((1 / 100) * $forcePKMN["end_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] - ($forcePKMN["agi_pkmn_2"] * 0.1));
             }
-            if ($forcePKMN["int_pkmn_2"] < 0)
+            if ($forcePKMN["end_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] + ((1 / 100) * $forcePKMN["int_pkmn_2"]));  
-            }
-            else
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] - ((1 / 100) * $forcePKMN["int_pkmn_2"]));  
-            }
-            if ($forcePKMN["luck_pkmn_2"] < 0)
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["luck"] = floor($forcePKMN["luck_pkmn"] + ((1 / 100) * $forcePKMN["luck_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] + ($forcePKMN["end_pkmn_2"] * 0.1));
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["luck_pkmn"] - ((1 / 100) * $forcePKMN["luck_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] - ($forcePKMN["end_pkmn_2"] * 0.1)); 
             }
-            if ($forcePKMN["def_pkmn_2"] < 0)
+            if ($forcePKMN["int_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] + ((1 / 100) * $forcePKMN["def_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] + ($forcePKMN["int_pkmn_2"] * 0.1));
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] - ((1 / 100) * $forcePKMN["def_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] - ($forcePKMN["int_pkmn_2"] * 0.1));
+            }
+            if ($forcePKMN["luck_pkmn_2"] > 0)
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["luck"] = floor($forcePKMN["luck_pkmn"] + ($forcePKMN["luck_pkmn_2"] * 0.1));
+            }
+            else
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["luck"] = floor($forcePKMN["luck_pkmn"] - ($forcePKMN["luck_pkmn_2"] * 0.1));
+            }
+            if ($forcePKMN["def_pkmn_2"] > 0)
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] + ($forcePKMN["def_pkmn_2"] * 0.1));
+            }
+            else
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] - ($forcePKMN["def_pkmn_2"] * 0.1));
             }
 
         }
@@ -126,55 +127,60 @@ class Math
         $forcePKMNs = UserDataRetrievalSession::getCPUDaemonStatsFirst();
         foreach ($forcePKMNs as $forcePKMN)
         {
-            if ($forcePKMN["force_pkmn_2"] < 0)
+            if ($forcePKMN["force_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] + ((1 / 100) * $forcePKMN["force_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] + ((1 / 10) * $forcePKMN["force_pkmn_2"]));
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] - ((1 / 100) * $forcePKMN["force_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["force_pkmn"] - ((1 / 10) * $forcePKMN["force_pkmn_2"]));  
             }
-            if ($forcePKMN["agi_pkmn_2"] < 0)
+            if ($forcePKMN["agi_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] + ((1 / 100) * $forcePKMN["agi_pkmn_2"]));  
-            }
-            else
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] - ((1 / 100) * $forcePKMN["agi_pkmn_2"]));  
-            }
-            if ($forcePKMN["end_pkmn_2"] < 0)
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] + ((1 / 100) * $forcePKMN["end_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] + ((1 / 10) * $forcePKMN["agi_pkmn_2"]));  
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] - ((1 / 100) * $forcePKMN["end_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["agi"] = floor($forcePKMN["agi_pkmn"] - ((1 / 10) * $forcePKMN["agi_pkmn_2"]));  
             }
-            if ($forcePKMN["int_pkmn_2"] < 0)
+            if ($forcePKMN["end_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] + ((1 / 100) * $forcePKMN["int_pkmn_2"]));  
-            }
-            else
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] - ((1 / 100) * $forcePKMN["int_pkmn_2"]));  
-            }
-            if ($forcePKMN["luck_pkmn_2"] < 0)
-            {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["luck"] = floor($forcePKMN["luck_pkmn"] + ((1 / 100) * $forcePKMN["luck_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] + ((1 / 10) * $forcePKMN["end_pkmn_2"]));  
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["luck_pkmn"] - ((1 / 100) * $forcePKMN["luck_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["end"] = floor($forcePKMN["end_pkmn"] - ((1 / 10) * $forcePKMN["end_pkmn_2"]));  
             }
-            if ($forcePKMN["def_pkmn_2"] < 0)
+            if ($forcePKMN["int_pkmn_2"] > 0)
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] + ((1 / 100) * $forcePKMN["def_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] + ((1 / 10) * $forcePKMN["int_pkmn_2"]));  
             }
             else
             {
-                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] - ((1 / 100) * $forcePKMN["def_pkmn_2"]));  
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["int"] = floor($forcePKMN["int_pkmn"] - ((1 / 10) * $forcePKMN["int_pkmn_2"]));  
+            }
+            if ($forcePKMN["luck_pkmn_2"] > 0)
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["luck"] = floor($forcePKMN["luck_pkmn"] + ((1 / 10) * $forcePKMN["luck_pkmn_2"]));  
+            }
+            else
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["for"] = floor($forcePKMN["luck_pkmn"] - ((1 / 10) * $forcePKMN["luck_pkmn_2"]));  
+            }
+            if ($forcePKMN["def_pkmn_2"] > 0)
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] + ((1 / 10) * $forcePKMN["def_pkmn_2"]));  
+            }
+            else
+            {
+                $calcFor[$forcePKMN["ordre_pkmn"]][$forcePKMN["nom_pkm"]]["def"] = floor($forcePKMN["def_pkmn"] - ((1 / 10) * $forcePKMN["def_pkmn_2"]));  
             }
         }
         return $calcFor;
+    }
+
+    static public function calcDmgPhys($skillPotency)
+    {
+        $dmg = floor();
     }
 }
