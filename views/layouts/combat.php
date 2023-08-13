c845aa3553df6d2b8d75a6a36fdf9d3434cbf2b4
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-08-13 17:55:48 +0200

    muted useless script

--- views/layouts/combat.php
+++ views/layouts/combat.php
@@ -5,9 +5,8 @@
         <title>Welcome !</title>
         <link rel="stylesheet" href="public/styles/combat.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merienda:wght@300;400;500;600;700;800;900&display=swap">
-        <script defer src="public/js/combat.js"> </script>
+        <!-- <script defer src="public/js/combat.js"> </script> -->
 
-</script>
     </head>
     <body>
         <div class="wrapper">
