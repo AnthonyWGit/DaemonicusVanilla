156a273530866615c87c5951099d959dc87187f2
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-08-13 17:56:19 +0200

    added hp bar and skill in url

--- views/templates/gameCombat.php
+++ views/templates/gameCombat.php
@@ -2,9 +2,9 @@
 <div class="wrapperInside">
     <div class="player">
         <p id="deamonPlayer"> <?= $daemon[0]["nom_pkm"] ?> Level <?= $daemonPlayerLevel ?></p>
-
+        <p id="deamonPlayerHP"> <?= $_SESSION["playerDaemonCurrentHP"] ?> HP </p>
         <div class="hp-bar">
-            <div class="hp-fill">
+            <div class="hp-fill" >
 
             </div>
         </div>
@@ -12,25 +12,26 @@
 
     <div class="cpu">
         <p id="deamonCPU"><?= $daemonCPU[0]["nom_pkm"] ?> Level <?= $daemonCPULevel ?></p>
+        <p id="deamonPlayerHP"> <?= $_SESSION["CPUDaemonCurrentHP"] ?> HP </p>
         <div class="hp-bar">
-            <div class="hp-fill">
+            <div class="hp-fill" id="hp_fill_cpu">
                 
             </div>
         </div>
     </div>
-    
 </div>
-<div class="actions" id="actions_menu">
-    <p class="title">What do you want to do ?</p> 
-    <div class="actionsElement">
-        <p id="daemon_skill">Daemon action</p>
-        <p id="item">Use item</p>
-        <p id="switch">Switch Daemon</p>
-        <p id="flee">Flee !</p>      
+
+    <div class="actions" id="actions_menu">
+        <p class="title">What do you want to do ?</p> 
+        <div class="actionsElement">
+            <p id="daemon_skill">Daemon action</p>
+            <p id="item">Use item</p>
+            <p id="switch">Switch Daemon</p>
+            <p id="flee">Flee !</p>      
+        </div>
     </div>
-
-
 </div>
+
 
 
 <script>
@@ -49,13 +50,19 @@
             function(skillText) 
                 {
                     let newParagraph = document.createElement("p");
-                    newParagraph.textContent = skillText;
                     actionsElement.appendChild(newParagraph);
+
+                    let link = document.createElement("a");
+                    link.textContent = skillText
+                    link.href = "game.php?combat=skill&" + skillText
+
+                    newParagraph.appendChild(link)
                 }
         );
         
     }
 
+    //Query selectorss 
     let actionsElement = document.querySelector(".actionsElement");
     const daemonSkills = document.querySelector("#daemon_skill");
     const item = document.querySelector("#item");
@@ -63,10 +70,14 @@
     const flee = document.querySelector("#flee");
     const titleMenu = document.querySelector("p.title");
     const actionsMenu = document.querySelector("#actions_menu");
+    const hpFillCPU = document.querySelector("#hp_fill_cpu");
+
+    let currentCPUHP = <?php echo $jsonCurrentCPUHp; ?>;
+    let arraySkillsJson = <?php echo $arrayOfSkillsJson; ?>;
 
     daemonSkills.addEventListener("click", targetElements);
-    let initiative = "<?php echo $initiative; ?>";
-    let arraySkillsJson = <?php echo $arrayOfSkillsJson; ?>;
+    hpFillCPU.style.width = currentCPUHP
+
     console.log(arraySkillsJson);
 </script>
 
